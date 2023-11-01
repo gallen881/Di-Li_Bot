bf95b510f4b2a045419a2982c3feb92b563521fa
Gallen Wang <gallen.yes@gmail.com>
2023-11-02 00:31:21 +0800

    add README information

---
+++ README.md
@@ -0,0 +1,9 @@
+# Di-Li Bot
+Use Discord to send and receive message from Line
+
+## How to use
+1. Create a Line bot(messaging API), and get the channel access token and channel secret.
+2. Create a Discord bot, and get the discord bot token.
+3. Run `setup.py`, and enter the necessary informations.
+4. Run `pip install -r requirements.txt`.
+4. Run `Dbot.py` and `Lbot.py`.
---
+++ setup.py
@@ -0,0 +1,57 @@
+import json
+
+line_assess_token = input('Enter your Line Bot channel assess token:\n')
+line_channel_secret = input('Enter your Line Bot channel secret:\n')
+port = int(input('Enter your Line Bot webhook port:\n'))
+discord_token = input('Enter your Discord Bot token:\n')
+discord_prefix = input('Enter your Discord Bot prefix:\n')
+discord_guild_id = int(input('Enter your Discord guild id:\n'))
+
+config = {
+    "LineBot": {
+        "ACCESS_TOKEN": line_assess_token,
+        "CHANNEL_SECRET": line_channel_secret,
+        "host": "0.0.0.0",
+        "port": port
+    },
+    "DiscordBot": {
+        "token": discord_token,
+        "prefix": discord_prefix,
+        "guild_id": discord_guild_id
+    }
+}
+
+with open('config.json', 'w') as file:
+    json.dump(config, file, indent=4)
+
+data = {
+    "last_timestamp": {
+        "message": 0,
+        "file": 0,
+        "image": 0,
+        "join": 0
+    },
+    "user_table": {},
+    "group_table": {}
+}
+
+with open('data.json', 'w') as file:
+    json.dump(data, file, indent=4)
+
+switcher = {
+    "message": {
+        "timestamp": 0
+    },
+    "join": {
+        "timestamp": 0
+    },
+    "file": {
+        "timestamp": 0
+    },
+    "image": {
+        "timestamp": 0
+    }
+}
+
+with open('switcher.json', 'w') as file:
+    json.dump(switcher, file, indent=4)
